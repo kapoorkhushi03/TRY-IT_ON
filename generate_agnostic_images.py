ff86dea9492bc3ed3276ab615a2c655a6ae9939f
khushi kapoor <140336136+kapoorkhushi03@users.noreply.github.com>
2024-07-15 20:51:24 +0530

    Add files via upload

---
+++ generate_agnostic_images.py
@@ -0,0 +1,102 @@
+import json
+from os import path as osp
+import os
+
+import numpy as np
+from PIL import Image, ImageDraw
+
+import argparse
+from tqdm import tqdm
+
+
+def get_img_agnostic(img, parse, pose_data, w=768, h=1024):
+    parse_array = np.array(parse)
+    parse_head = ((parse_array == 4).astype(np.float32) +
+                  (parse_array == 13).astype(np.float32))
+    parse_lower = ((parse_array == 9).astype(np.float32) +
+                   (parse_array == 12).astype(np.float32) +
+                   (parse_array == 16).astype(np.float32) +
+                   (parse_array == 17).astype(np.float32) +
+                   (parse_array == 18).astype(np.float32) +
+                   (parse_array == 19).astype(np.float32))
+
+    r = 20
+    agnostic = img.copy()
+    agnostic_draw = ImageDraw.Draw(agnostic)
+
+    length_a = np.linalg.norm(pose_data[5] - pose_data[2])
+    length_b = np.linalg.norm(pose_data[12] - pose_data[9])
+    point = (pose_data[9] + pose_data[12]) / 2
+    pose_data[9] = point + (pose_data[9] - point) / length_b * length_a
+    pose_data[12] = point + (pose_data[12] - point) / length_b * length_a
+
+    # mask arms
+    agnostic_draw.line([tuple(pose_data[i]) for i in [2, 5]], 'gray', width=r*10)
+    for i in [2, 5]:
+        pointx, pointy = pose_data[i]
+        agnostic_draw.ellipse((pointx-r*5, pointy-r*5, pointx+r*5, pointy+r*5), 'gray', 'gray')
+    for i in [3, 4, 6, 7]:
+        if (pose_data[i - 1, 0] == 0.0 and pose_data[i - 1, 1] == 0.0) or (pose_data[i, 0] == 0.0 and pose_data[i, 1] == 0.0):
+            continue
+        agnostic_draw.line([tuple(pose_data[j]) for j in [i - 1, i]], 'gray', width=r*10)
+        pointx, pointy = pose_data[i]
+        agnostic_draw.ellipse((pointx-r*5, pointy-r*5, pointx+r*5, pointy+r*5), 'gray', 'gray')
+
+    # mask torso
+    for i in [9, 12]:
+        pointx, pointy = pose_data[i]
+        agnostic_draw.ellipse((pointx-r*3, pointy-r*6, pointx+r*3, pointy+r*6), 'gray', 'gray')
+    agnostic_draw.line([tuple(pose_data[i]) for i in [2, 9]], 'gray', width=r*6)
+    agnostic_draw.line([tuple(pose_data[i]) for i in [5, 12]], 'gray', width=r*6)
+    agnostic_draw.line([tuple(pose_data[i]) for i in [9, 12]], 'gray', width=r*12)
+    agnostic_draw.polygon([tuple(pose_data[i]) for i in [2, 5, 12, 9]], 'gray', 'gray')
+
+    # mask neck
+    pointx, pointy = pose_data[1]
+    agnostic_draw.rectangle((pointx-r*7, pointy-r*7, pointx+r*7, pointy+r*7), 'gray', 'gray')
+    agnostic.paste(img, None, Image.fromarray(np.uint8(parse_head * 255), 'L'))
+    agnostic.paste(img, None, Image.fromarray(np.uint8(parse_lower * 255), 'L'))
+
+    return agnostic
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default='/Users/khushikapoor/Desktop/project/data', help="dataset dir")
+    parser.add_argument('--output_path', type=str, default='/Users/khushikapoor/Desktop/project/output/output_agnostic', help="output dir")
+
+    args = parser.parse_args()
+    data_path = args.data_path
+    output_path = args.output_path
+    
+    os.makedirs(output_path, exist_ok=True)
+    
+    for im_name in tqdm(os.listdir(osp.join(data_path, 'image'))):
+        
+        # load image
+        img_name = im_name
+        img = Image.open(osp.join(data_path, 'image', img_name))
+
+        # load pose image
+        pose_name = im_name.replace('.jpg', '_keypoints.json')
+        
+        try:
+            with open(osp.join(data_path, 'openpose_json', pose_name), 'r') as f:
+                pose_label = json.load(f)
+                pose_data = pose_label['people'][0]['pose_keypoints_2d']
+                pose_data = np.array(pose_data)
+                pose_data = pose_data.reshape((-1, 3))[:, :2]
+        except IndexError:
+            print(pose_name)
+            continue
+
+        # load parsing image
+        parse_name = im_name.replace('.jpg', '.png')
+        parse = Image.open(osp.join(data_path, 'image-parse-v3', parse_name))
+
+        agnostic = get_img_agnostic(img, parse, pose_data)
+        
+        # Save the agnostic image
+        output_file = osp.join(output_path, img_name)
+        agnostic.save(output_file)
+        print(f"Saved {output_file}")
